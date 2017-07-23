8a910b256bebc97389d83a962d55cab80a7d1f5e
Yuriy Kushnir <yurko7@hotmail.com>
2017-07-23 19:54:10 +0300

    Replace static Compiler class with extension methods.

---
+++ math-expression/Extensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace YuKu.MathExpression
+{
+    public static class Extensions
+    {
+        public static TDelegate Compile<TDelegate>(this String mathExpression, params String[] parameterNames)
+        {
+            using (var reader = new StringReader(mathExpression))
+            {
+                return reader.Compile<TDelegate>(parameterNames);
+            }
+        }
+
+        public static TDelegate Compile<TDelegate>(this TextReader mathExpression, params String[] parameterNames)
+        {
+            ParameterExpression[] parameters = parameterNames
+                .Select(param => Expression.Parameter(typeof(Double), param))
+                .ToArray();
+            var lexer = new Lexer(mathExpression);
+            var parser = new Parser();
+            Expression expression = parser.Parse(lexer, parameters);
+
+            Expression<TDelegate> lambda = Expression.Lambda<TDelegate>(expression, parameters);
+            return lambda.Compile();
+        }
+    }
+}
