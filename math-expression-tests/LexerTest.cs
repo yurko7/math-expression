57eeb4a9e9ba2bda354b661f4ffd4e7897f0a7c7
Yuriy Kushnir <yurko7@hotmail.com>
2017-10-10 00:12:01 +0300

    Add Lexer tests for empty and single token inputs.

---
+++ math-expression-tests/LexerTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace YuKu.MathExpression.Tests
+{
+    [TestClass]
+    public class LexerTest
+    {
+        [TestMethod]
+        public void EmptyInput()
+        {
+            using (var mathExpression = new StringReader(String.Empty))
+            {
+                using (var lexer = new Lexer(mathExpression))
+                {
+                    Assert.IsTrue(lexer.MoveNext());
+                    Assert.AreEqual(TokenType.EOF, lexer.Current.Type);
+                    Assert.IsFalse(lexer.MoveNext());
+                }
+            }
+        }
+
+        [TestMethod]
+        [JsonDataSource(@".\Data\SingleToken.json")]
+        public void SingleToken(String input, TokenType token, Boolean text)
+        {
+            using (var mathExpression = new StringReader(input))
+            {
+                using (var lexer = new Lexer(mathExpression))
+                {
+                    Assert.IsTrue(lexer.MoveNext());
+                    Assert.AreEqual(token, lexer.Current.Type);
+                    if (text)
+                    {
+                        Assert.AreEqual(input.Trim(), lexer.Current.Text);
+                    }
+                    Assert.IsTrue(lexer.MoveNext());
+                    Assert.AreEqual(TokenType.EOF, lexer.Current.Type);
+                    Assert.IsFalse(lexer.MoveNext());
+                }
+            }
+        }
+    }
+}
