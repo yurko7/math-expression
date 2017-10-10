9dd17f248e8408da99ed34dc0dece9220dcec45d
Yuriy Kushnir <yurko7@hotmail.com>
2017-10-10 09:13:36 +0300

    Implement equality comparison on Token struct.

--- math-expression/Token.cs
+++ math-expression/Token.cs
@@ -2,7 +2,7 @@
 
 namespace YuKu.MathExpression
 {
-    public struct Token
+    public struct Token : IEquatable<Token>
     {
         public Token(TokenType type, Int32 location, String text = null)
         {
@@ -11,11 +11,48 @@ namespace YuKu.MathExpression
             Text = text;
         }
 
+        public static Boolean operator ==(Token left, Token right)
+        {
+            return left.Equals(right);
+        }
+
+        public static Boolean operator !=(Token left, Token right)
+        {
+            return !left.Equals(right);
+        }
+
         public TokenType Type { get; }
 
         public Int32 Location { get; }
 
         public String Text { get; }
+
+        public override Boolean Equals(Object obj)
+        {
+            if (ReferenceEquals(obj, null))
+            {
+                return false;
+            }
+            return obj is Token token && Equals(token);
+        }
+
+        public Boolean Equals(Token other)
+        {
+            return Type == other.Type
+                && Location == other.Location
+                && String.Equals(Text, other.Text, StringComparison.Ordinal);
+        }
+
+        public override Int32 GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = (Int32) Type;
+                hashCode = (hashCode * 397) ^ Location;
+                hashCode = (hashCode * 397) ^ (Text != null ? StringComparer.Ordinal.GetHashCode(Text) : 0);
+                return hashCode;
+            }
+        }
 
         public override String ToString()
         {
